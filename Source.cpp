015d8d399e6fb802318986b8bbf0f576accb8225
Vlad Kravtsov <31658721+MegaProger228@users.noreply.github.com>
2017-10-14 10:13:08 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,38 +1,39 @@
 #define _CRT_SECURE_NO_WARNINGS
+#include "stdafx.h"
+
 #include <stdio.h>
 #include <conio.h>
-
 
 #define DYUMS  39370.08
 #define YARDS 1093.61
 
-
 using namespace std;
 
 int main()
+
 {
+
 	printf("Type count of km \n");
-	double yards = 0.0, dyums = 0.0/*, YARDS = 1093.61, DYUMS = 39370.08*/;
-	double kilometers;
-	scanf("%f", &kilometers); // %d - для ввода целых, %f - для ввода вещественных
+	float yards = 0.0, dyums = 0.0/*, YARDS = 1093.61, DYUMS = 39370.08*/;
+	float kilometers;
+	scanf_s("%f", &kilometers); 
 
-
-	//  Получение ярдов
+	// РџРѕР»СѓС‡РµРЅРёРµ Рё РІС‹РІРѕРґ СЏСЂРґРѕРІ
 	yards = kilometers * YARDS;
 	printf("Count of yards = ");
 	printf("%f", yards);
 	printf("\n");
 
 
-	//  Получение дюймов
+	// РџРѕР»СѓС‡РµРЅРёРµ Рё РІС‹РІРѕРґ РґСЋР№РјРѕРІ
 	dyums = kilometers * DYUMS;
 	printf("Count of dyums = ");
 	printf("%f", dyums);
 	printf("\n");
 
 
-
 	_getch();
-	return 0;
-}
 
+	return 0;
+
+}
